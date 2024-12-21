42276a80ba0c71ed3f6172a0faf9a9c9e4997d52
RuchiraGit2002 <ruchirapriyankara7777@gmail.com>
2024-12-21 22:01:51 +0530

    Initial Commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] randomNumbers = new int[5];
+        Random random = new Random();
+
+        // Sets each element to a random value between 0 and 99
+        Arrays.setAll(randomNumbers, i -> random.nextInt(100));
+
+        System.out.println(Arrays.toString(randomNumbers)); // Output: [e.g., 12, 47, 86, 33, 5]
+    }
+}
+
+
---
+++ OneDArray.java
@@ -0,0 +1,12 @@
+public class OneDArray {
+    public static void main(String args[]){
+        //float[] marks = {80.56f,75.60f,65.25f};
+        //System.out.println(marks[2]);
+
+        int[] distance = new int[5];
+        distance[0] = 100;
+        System.out.println(distance[0]);
+        System.out.println(distance[3]);
+
+    }
+}
